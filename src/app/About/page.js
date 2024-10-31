4a4dcb6be36b01f12fceffe8f8755fb6bf7584f7
fahadalijs <fahad9827022@gmail.com>
2024-10-31 18:26:36 +0500

    next-routing

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -4,10 +4,10 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    <h1>Next Routing</h1>
+    <h1>About</h1>
      <button className='bg-blue-500'><Link href={"About"}>About</Link></button> <br/>           
-    <button><Link href={"Contact/Person"}>Contact</Link></button> <br/>
-    <button className='hover:bg-blue-600'><Link href={"Login"}>Login</Link></button> <br/>
+     <button><Link href={"Contact"}>Contact</Link></button> <br/>
+     <button className='hover:bg-blue-600'><Link href={"Login"}>Login</Link></button> <br/>
     </>
   )
 }
--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -4,7 +4,7 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    <h1>Next Routing</h1>
+    <h1>Contact</h1>
      <button className='bg-blue-500'><Link href={"About"}>About</Link></button> <br/>           
     <button><Link href={"Contact/Person"}>Contact</Link></button> <br/>
     <button className='hover:bg-blue-600'><Link href={"Login"}>Login</Link></button> <br/>
